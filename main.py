b35c072bec502dfea6358c6e085b53895eb9b456
Cloud <ruben.as532@gmail.com>
2024-01-19 10:31:46 +0100

    Players now can leave casino

--- main.py
+++ main.py
@@ -12,10 +12,19 @@ def enterCasino(input):
     else:
         players[input[0]] = 0
 
+def leaveCasino(input):
+    if input[0] in players:
+        print(input[0]+" has won "+str(players[input[0]]))
+        players.pop(input[0])
+    else:
+        print(input[0]+" is not in the casino")
+
 def useOption(option):
     input = option.split()
     if "enters" in input[1]:
         enterCasino(input)
+    elif "leaves" in input[1]:
+        leaveCasino(input)
 
 while option != "" :
     useOption(option)
