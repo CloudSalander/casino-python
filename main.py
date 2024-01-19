e7461ce81a52c83d13e61a3f5703e0f43c04824c
Cloud <ruben.as532@gmail.com>
2024-01-19 10:11:34 +0100

    Now players can enter to the casino

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+"""
+TO-DO:
+- Input validation
+- Add constant values
+"""
 players = {}
-option = ":)"
+option = input()
+
+def enterCasino(input):
+    if input[0] in players:
+        print(input[0]+ " is already in the casino")
+    else:
+        players[input[0]] = 0
+
+def useOption(option):
+    input = option.split()
+    if "enters" in input[1]:
+        enterCasino(input)
 
 while option != "" :
+    useOption(option)
     option = input()
+
+print(players)
